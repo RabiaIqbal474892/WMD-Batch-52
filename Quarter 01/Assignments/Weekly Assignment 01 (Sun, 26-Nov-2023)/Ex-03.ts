fa8e1273b84ecf2abb37a3f4abf237813b84f232
RabiaIqbal474892 <151126008+RabiaIqbal474892@users.noreply.github.com>
2023-12-08 11:37:12 +0500

    Add files via upload

---
+++ Quarter 01/Assignments/Weekly Assignment 01 (Sun, 26-Nov-2023)/Ex-03.ts
@@ -0,0 +1,36 @@
+/*
+Exercise 3: Write a program that checks if a given number is positive, negative, or zero.
+*/
+
+function checknumber(numberCheck: number): string {
+  if (numberCheck > 0) {
+    return "Postive";
+  } else if (numberCheck < 0) {
+    return "Negative";
+  } else {
+    return "Zero";
+  }
+}
+
+const inputNumber: number = 9;
+const result: string = checknumber(inputNumber);
+
+console.log(`Given number is ${inputNumber} which is ${result}.`);
+
+// Method 2: with arrow function
+
+const checkNumber = (num: number) => {
+  if (num > 0) {
+    console.log(`Number ${num} is Positive`);
+  } else if (num < 0) {
+    console.log(`Number ${num} is Negative`);
+  } else {
+    return console.log(`Number is Zero`);
+  }
+};
+
+checkNumber(1);
+checkNumber(-6);
+checkNumber(0);
+
+export {};
