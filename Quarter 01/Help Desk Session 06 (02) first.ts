28c6a16d5a2f08fa7fb126f9cb6f420f177a36bd
RabiaIqbal474892 <151126008+RabiaIqbal474892@users.noreply.github.com>
2024-01-10 15:47:08 +0500

    Add files via upload

---
+++ Quarter 01/Help Desk Session 06 (02) first.ts
@@ -0,0 +1,11 @@
+// step03a_modules
+
+export const a = 30;
+
+export const b = 34;
+
+const c = 100;
+
+export default c;
+
+// export default file ma sirf 1 he ho sakti hai
---
+++ Quarter 01/Help Desk Session 06 (04) third.ts
@@ -0,0 +1,4 @@
+// script type not module type bacause no export {}
+const a = 10;
+
+// is script k jtne b variable hnge, wo global scope hnge
