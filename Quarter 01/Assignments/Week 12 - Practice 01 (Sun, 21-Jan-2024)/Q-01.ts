83c423a4786b0d882f34f0f4d67253471107e8eb
RabiaIqbal474892 <151126008+RabiaIqbal474892@users.noreply.github.com>
2024-01-30 00:45:34 +0500

    Delete Quarter 01/Assignments/Week 12 - Practice 01 (Sun, 21-Jan-2024) directory

--- Quarter 01/Assignments/Week 12 - Practice 01 (Sun, 21-Jan-2024)/Q-01.ts
+++
@@ -1,9 +0,0 @@
-// Basics
-
-// 1. Variable Assignment
-// Create a variable named name and assign it a string value.
-
-// Declare a variable named 'name' and assign it to a string value
-let name2: string = "Rabiaa Iqbal";
-
-console.log(name2);
