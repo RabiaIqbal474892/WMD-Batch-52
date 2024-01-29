295c577c70c7e1c78ccdfff44b04bf73cb80b735
RabiaIqbal474892 <151126008+RabiaIqbal474892@users.noreply.github.com>
2024-01-29 11:01:36 +0500

    Add files via upload

---
+++ Quarter 01/Assignments/Assignment (45 Questions)/Q-07/Q-07.ts
@@ -0,0 +1,44 @@
+/*
+Q # 07
+Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+*/
+
+// Addition
+
+const additionResult = 4 + 4;
+
+console.log("Addition Result: 4 + 4 = " + additionResult);
+
+const additionResult2 = 5 + 3;
+
+console.log(`Addition: 5 + 3 = ${additionResult2}`);
+
+// Subtraction
+
+const subtractionResult = 16 - 8;
+
+console.log("Subtraction Result: 16 - 8 = " + subtractionResult);
+
+const subtractionResult2 = 10 - 2;
+
+console.log(`Subtraction: 10 - 2 = ${subtractionResult2}`);
+
+// Multiplication
+
+const multiplicationResult = 8 * 1;
+
+console.log("Multiplication Result: 8 * 1 = " + multiplicationResult);
+
+const multiplicationResult2 = 4 * 2;
+
+console.log(`Multiplication: 4 * 2 = ${multiplicationResult2}`);
+
+// Division
+
+const divisionResult = 64 / 8;
+
+console.log("Division Result: 64 / 8 = " + divisionResult);
+
+const divisionResult2 = 16 / 2;
+
+console.log(`Division: 16 / 2 = ${divisionResult2}`);
