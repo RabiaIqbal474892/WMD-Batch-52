dc61dade3d339afbf74bd3076b4d4b330626c02b
RabiaIqbal474892 <151126008+RabiaIqbal474892@users.noreply.github.com>
2024-01-29 11:49:50 +0500

    Add files via upload

---
+++ Quarter 01/Assignments/Assignment (45 Questions)/Q-42/Q-42.ts
@@ -0,0 +1,37 @@
+/*
+Q # 42
+Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+*/
+
+// Array of magician’s names
+const magicianNames: string[] = [
+  "Harry Potter",
+  "David Copperfield",
+  "Hermione Granger",
+  "Professor Albus Dumbledore",
+  "Ron Weasley",
+];
+
+function showMagicians(magicians: string[]): void {
+  for (const magician of magicians) {
+    console.log(magician);
+  }
+}
+
+function makeGreat(magicians: string[]): string[] {
+  const greatMagicians: string[] = magicians.map(
+    (magician) => `the Great ${magician}`
+  );
+  return greatMagicians;
+}
+
+// Modify the array to add "the Great" to each magician's name
+const greatMagicianNames: string[] = makeGreat(magicianNames);
+
+// Call the function to show the original magician's names
+console.log("Original Magicians:");
+showMagicians(magicianNames);
+
+// Call the function to show the great magician's names
+console.log("\nGreat Magicians:");
+showMagicians(greatMagicianNames);
