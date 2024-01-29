c0f6fdea0714d7448c1de77f234911555c9d157f
RabiaIqbal474892 <151126008+RabiaIqbal474892@users.noreply.github.com>
2024-01-29 10:59:07 +0500

    Add files via upload

---
+++ Quarter 01/Assignments/Assignment (45 Questions)/Q-05/Q-05.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+Q # 05
+Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+Then compose your message and store it in a new variable called message. Print your message.
+*/
+// Quaid-e-Azam once said, "There are two powers in the world; one is the sword and the other is the pen. There is a great competition and rivalry between the two. There is a third power stronger than both, that of the women."
+// Quaid-e-Azam Muhammad Ali Jinnah once said, "The great majority of us are Muslims. We follow the teachings of the Prophet Mohammed (may peace be upon him). We are members of the brotherhood of Islam in which all are equal in rights, dignity and self-respect. Consequently, we have a special and a very deep sense of unity. But make no mistake: Pakistan is not a theocracy or anything like it."
+// Quaid-e-Azam once said, "No struggle can ever succeed without women participating side by side with men."
+// Quaid-e-Azam Muhammad Ali Jinnah once said, "Expect the best, prepare for the worst."
+// Allama Muhammad Iqbal once said, "Be aware of your own worth, use all of your power to achieve it. Create an ocean from a dewdrop. Do not beg for light from the moon, obtain it from the spark within you."
+// Method 1
+let famous_person = "Quaid-e-Azam Muhammad Ali Jinnah";
+let message = famous_person + ' once said, "Expect the best, prepare for the worst."';
+console.log(message);
+// Method 2
+// Store the famous person's name
+let famous_person2 = "Quaid-e-Azam Muhammad Ali Jinnah";
+// Compose the message
+let message2 = `${famous_person2} once said, "Expect the best, prepare for the worst."`;
+// Print the message
+console.log(message2);
