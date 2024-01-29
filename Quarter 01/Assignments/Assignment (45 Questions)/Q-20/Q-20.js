dbfb0c5673ad1184b8756fd84bdf2940e24416cb
RabiaIqbal474892 <151126008+RabiaIqbal474892@users.noreply.github.com>
2024-01-29 11:19:06 +0500

    Add files via upload

---
+++ Quarter 01/Assignments/Assignment (45 Questions)/Q-20/Q-20.js
@@ -0,0 +1,16 @@
+"use strict";
+/*
+Q # 20
+Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+*/
+const countries = [
+    "Pakistan",
+    "Australia",
+    "Saudi Arabia",
+    "Dubai",
+    "England",
+];
+console.log("List of Countries:");
+for (let country of countries) {
+    console.log(country);
+}
---
+++ Quarter 01/Assignments/Assignment (45 Questions)/Q-20/Q-20.ts
@@ -0,0 +1,18 @@
+/*
+Q # 20
+Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+*/
+
+const countries: string[] = [
+  "Pakistan",
+  "Australia",
+  "Saudi Arabia",
+  "Dubai",
+  "England",
+];
+
+console.log("List of Countries:");
+
+for (let country of countries) {
+  console.log(country);
+}
