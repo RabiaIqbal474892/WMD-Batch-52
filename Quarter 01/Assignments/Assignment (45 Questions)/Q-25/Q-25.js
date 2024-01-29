f2f3981167f1a7eb4f4aebb681d99abfa5d264a0
RabiaIqbal474892 <151126008+RabiaIqbal474892@users.noreply.github.com>
2024-01-29 11:23:59 +0500

    Add files via upload

---
+++ Quarter 01/Assignments/Assignment (45 Questions)/Q-25/Q-25.js
@@ -0,0 +1,27 @@
+"use strict";
+/*
+Q # 25
+Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+• Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+
+• Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+*/
+let alien_color = "green";
+if (alien_color == "green") {
+    console.log("Congratulations ! You just earned 5 points.");
+}
+// second version
+alien_color = "blue";
+if (alien_color == "green") {
+    console.log("Congratulations ! You just earned 5 points.");
+}
+// Version that passes the if test (alien_color is 'green')
+let alien_color_pass = "green";
+if (alien_color_pass === "green") {
+    console.log("Congratulations! You just earned 5 points.");
+}
+// Version that fails the if test (alien_color is 'yellow' or 'red')
+let alien_color_fail = "red";
+if (alien_color_fail === "green") {
+    console.log("Congratulations! You just earned 5 points.");
+}
