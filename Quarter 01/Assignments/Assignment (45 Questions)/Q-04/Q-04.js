0179e76b3cf5e4bfe2fceaaa5f48a54a74eef790
RabiaIqbal474892 <151126008+RabiaIqbal474892@users.noreply.github.com>
2024-01-29 10:58:14 +0500

    Add files via upload

---
+++ Quarter 01/Assignments/Assignment (45 Questions)/Q-04/Q-04.js
@@ -0,0 +1,19 @@
+"use strict";
+/*
+Q # 04
+Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+Albert Einstein once said, "A person who never made a mistake never tried anything new."
+*/
+// Quaid-e-Azam once said, "There are two powers in the world; one is the sword and the other is the pen. There is a great competition and rivalry between the two. There is a third power stronger than both, that of the women."
+// Quaid-e-Azam once said, "The great majority of us are Muslims. We follow the teachings of the Prophet Mohammed (may peace be upon him). We are members of the brotherhood of Islam in which all are equal in rights, dignity and self-respect. Consequently, we have a special and a very deep sense of unity. But make no mistake: Pakistan is not a theocracy or anything like it."
+// Quaid-e-Azam once said, "No struggle can ever succeed without women participating side by side with men."
+// Quaid-e-Azam Muhammad Ali Jinnah once said, "Expect the best, prepare for the worst."
+// Allama Muhammad Iqbal once said, "Be aware of your own worth, use all of your power to achieve it. Create an ocean from a dewdrop. Do not beg for light from the moon, obtain it from the spark within you."
+console.log('Quaid-e-Azam Muhammad Ali Jinnah once said, "Expect the best, prepare for the worst."');
+// Method 2
+// Store the quote and its author
+let quote = "Be aware of your own worth, use all of your power to achieve it. Create an ocean from a dewdrop. Do not beg for light from the moon, obtain it from the spark within you.";
+let author = "Allama Muhammad Iqbal";
+// Print the quote and its author
+console.log(`${author} once said, "${quote}"`);
