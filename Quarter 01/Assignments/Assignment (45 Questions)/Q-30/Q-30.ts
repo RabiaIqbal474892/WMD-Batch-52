555e4c6939e0ecb34b5a7ee259b5a69fe9c7f7cf
RabiaIqbal474892 <151126008+RabiaIqbal474892@users.noreply.github.com>
2024-01-29 11:38:40 +0500

    Add files via upload

---
+++ Quarter 01/Assignments/Assignment (45 Questions)/Q-30/Q-30.ts
@@ -0,0 +1,27 @@
+/*
+Q # 30
+Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+• If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+
+• Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+*/
+
+const userNames: string[] = [
+  "admin",
+  "Muhammad Ali",
+  "Mustufa Khuram",
+  "Muhammad Kareem",
+  "Muhammad Wahab",
+];
+
+function greetUsers(): void {
+  for (const i in userNames) {
+    if (userNames[i].toLowerCase() === "admin") {
+      console.log(`Hi admin, would you like to see a status report?`);
+    } else {
+      console.log(`Hi ${userNames[i]}, thank you for logging in again.`);
+    }
+  }
+}
+
+greetUsers();
