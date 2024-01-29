d9ba20244545af298ef3c6f3bd96177197d0915b
RabiaIqbal474892 <151126008+RabiaIqbal474892@users.noreply.github.com>
2024-01-29 11:18:10 +0500

    Add files via upload

---
+++ Quarter 01/Assignments/Assignment (45 Questions)/Q-19/Q-19.js
@@ -0,0 +1,10 @@
+"use strict";
+/*
+Q # 19
+Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+// Importing Guests List from exercise 15
+const Q_15_1 = require("./Q-15");
+console.log(`\nPrinting message indicating number of guests`);
+console.log(`We had finally invited ${Q_15_1.guestList.length} guest in Exercise 15`);
