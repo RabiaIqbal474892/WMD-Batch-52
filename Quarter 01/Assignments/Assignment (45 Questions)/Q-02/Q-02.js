5f9ce19c43bb7d29a10c003abfb5b1588c190f84
RabiaIqbal474892 <151126008+RabiaIqbal474892@users.noreply.github.com>
2024-01-29 10:55:52 +0500

    Add files via upload

---
+++ Quarter 01/Assignments/Assignment (45 Questions)/Q-02/Q-02.js
@@ -0,0 +1,14 @@
+/*
+Q # 02
+Personal Message: Store a person’s name in a variable, and print a message to that person.
+Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+*/
+// Store a person's name in a variable
+var personName = "Eric";
+// Print a personalized message
+console.log("Hello ".concat(personName, ", would you like to learn some TypeScript today?"));
+// Store a person's name in a variable
+var personName2 = "Rabiaa";
+var message = "Hi ".concat(personName2, ", would you like to learn some TypeScript today?");
+// Print a personalized message
+console.log(message);
---
+++ Quarter 01/Assignments/Assignment (45 Questions)/Q-02/Q-02.ts
@@ -0,0 +1,21 @@
+/*
+Q # 02
+Personal Message: Store a person’s name in a variable, and print a message to that person.
+Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+*/
+
+// Store a person's name in a variable
+let personName: string = "Eric";
+
+// Print a personalized message
+console.log(
+  `Hello ${personName}, would you like to learn some TypeScript today?`
+);
+
+// Store a person's name in a variable
+let personName2: string = "Rabiaa";
+
+let message: string = `Hi ${personName2}, would you like to learn some TypeScript today?`;
+
+// Print a personalized message
+console.log(message);
