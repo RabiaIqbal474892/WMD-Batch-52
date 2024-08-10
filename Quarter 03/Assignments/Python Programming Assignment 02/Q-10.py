38006fd8bc103054924a304633e1179d4ff9967f
RabiaIqbal474892 <151126008+RabiaIqbal474892@users.noreply.github.com>
2024-08-10 08:47:46 +0500

    Add files via upload

---
+++ Quarter 03/Assignments/Python Programming Assignment 02/Q-10.py
@@ -0,0 +1,9 @@
+# Get last element
+
+def get_last_element(lst):
+    # Print the last element in the list
+    print(lst[-1])
+
+# Example usage
+example_list = [10, 20, 30, 40]
+get_last_element(example_list)  # Output will be 40
